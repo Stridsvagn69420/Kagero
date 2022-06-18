4c3c1178c8d432915518b46cc855f5bfb40a5a2b
Stridsvagn69420 <Stridsvagn69420@users.noreply.github.com>
2022-06-18 04:24:43 +0200

    Orochi data types

---
+++ src/orochi/mod.rs
@@ -0,0 +1,105 @@
+use std::fmt::Display;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct Shuriken {
+    pub hash: String,
+    pub package: String,
+    pub version: [u8; 3],
+    pub platform: Platform,
+    pub arch: Arch,
+    pub path: String,
+    pub packtype: PackType
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum Platform {
+    Other,
+    Any,
+    Windows,
+    Linux
+}
+
+impl AsRef<str> for Platform {
+    fn as_ref(&self) -> &str {
+        match self {
+            Platform::Other => "other",
+            Platform::Any => "any",
+            Platform::Windows => "windows",
+            Platform::Linux => "linux"
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum Arch {
+    AARCH64,
+    ARM,
+    X86,
+    X86_64,
+    Runtime
+}
+
+impl AsRef<str> for Arch {
+    fn as_ref(&self) -> &str {
+        match self {
+            Arch::AARCH64 => "aarch64",
+            Arch::ARM => "arm",
+            Arch::X86 => "x86",
+            Arch::X86_64 => "x86_64",
+            Arch::Runtime => "runtime"
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum PackType {
+    Zip,
+    TarGz,
+    TarXz,
+    Exe,
+    Git
+}
+
+impl AsRef<str> for PackType {
+    fn as_ref(&self) -> &str {
+        match self {
+            PackType::Zip => "zip",
+            PackType::TarGz => "tar+gzip",
+            PackType::TarXz => "tar+xz",
+            PackType::Exe => "exe",
+            PackType::Git => "git",
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Katana {
+    pub uuid: String,
+    pub name: String,
+    pub root: String,
+    pub website: String,
+    pub maintainers: Vec<Maintainer>,
+    pub shurikens: Vec<Shuriken>
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Maintainer {
+    pub name: String,
+    pub email: String,
+    pub website: String
+}
+
+impl Display for Maintainer {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        if (self.email.len() > 0) && (self.website.len() > 0) {
+            write!(f, "{} <{}> ({})", self.name, self.email, self.website)
+        } else if self.email.len() > 0 {
+            write!(f, "{} <{}>", self.name, self.email)
+        } else if self.website.len() > 0 {
+            write!(f, "{} ({})", self.name, self.website)
+        } else {
+            write!(f, "{}", self.name)
+        }
+    }
+}
