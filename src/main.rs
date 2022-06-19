3faec999344c94760b3980d60f2f247522984abf
Stridsvagn69420 <67603294+Stridsvagn69420@users.noreply.github.com>
2022-06-19 15:43:29 +0200

    Merge Missing docs (#8)
    * Add docs for Orochi
    
    * Add docs and stdin to printer
    
    * Fix prompt issue and add docs to submodules
    
    * Use hyperlinks for fictional links, because cargo said so.
    
    * Broken examples... thank you, Copilot...
    
    Co-authored-by: Stridsvagn69420 <Stridsvagn69420@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use kagero::test;
 use kagero::printer::{Printer, Colors};
 
 fn main() {
-    test();
     let mut printer = Printer::default();
     printer.println("You're gay!", Colors::Green);
     printer.errorln("Wait, that's illegal!", Colors::Magenta);
+    let username = printer.prompt("Tell me, what's your name, mate?", Colors::CyanBright);
+    let input = printer.promptln(format!("Oh, hello there {}! Type in something...", username).as_str(), Colors::Yellow);
+    printer.writeln(input.as_str());
 }
