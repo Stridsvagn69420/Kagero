0292ee412b6fda472953375c8582c16d837c2453
Stridsvagn69420 <Stridsvagn69420@users.noreply.github.com>
2022-07-08 10:24:24 +0200

    Add HttpErrors enum

--- src/orochi/repository/mod.rs
+++ src/orochi/repository/mod.rs
@@ -1,3 +1,7 @@
+use thiserror::Error;
+use reqwest;
+use serde_json;
+
 /// FTP Repository implementation
 /// 
 /// This module implements utils for FTP repositores.
@@ -9,6 +13,24 @@ pub use self::ftp::FtpDownloader;
 /// This module implements utils for HTTP repositores.
 mod http;
 pub use self::http::HttpDownloader;
+
+/// HTTP-Downloader Errors
+///
+/// Errors that can occur when working with the [HttpDownloader]
+#[derive(Error, Debug)]
+pub enum HttpError {
+	/// Request Error
+	/// 
+	/// Translates to [reqwest::Error]]
+	#[error("an error occured during the request")]
+	RequestError(#[from] reqwest::Error),
+
+	/// Parsing Error
+	///
+	/// Translates to [serde_json::Error]]
+	#[error("json could not be parsed")]
+	ParsingError(#[from] serde_json::Error)
+}
 
 /// SSH Repository implementation
 /// 
